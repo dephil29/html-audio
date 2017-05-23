4a8b4f87ff655247c766fb3bfd3773f1aa1c5928
Eric Phillips <Eric Phillips>
2017-05-23 14:54:09 -0400

    removed songs

--- js/playNextVideo.js
+++ js/playNextVideo.js
@@ -5,7 +5,7 @@ function playNextVideo(){
   videoFileName = videoFileName[videoFileName.length-1];
   var currentIndex = vijyaList.indexOf(videoFileName);
   var nextIndex = (currentIndex+1) % vijyaList.length;
-  videoElement.src = 'videos/'+vijyaList[nextIndex];
+  videoElement.src = 'videos/' + vijyaList[nextIndex];
   videoElement.addEventListener('loadeddata', function() {
     videoElement.play();
 })
